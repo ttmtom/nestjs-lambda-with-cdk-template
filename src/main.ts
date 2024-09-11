cd6d8b3cda5b230888f88a24d7935e0399ddda24
Tom Tam <tom.tam@fwd.com>
2024-09-11 15:35:59 +0800

    nestjs init

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@codegenie/serverless-express';
+import express from 'express';
+import {AppModule} from "./app/app.module";
+
+
+let cachedServer;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    // nestApp.enableCors();
+
+    await nestApp.init();
+
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+
+  return cachedServer;
+}
+
+export const handler = async (event: any, context: any, callback: any) => {
+  const server = await bootstrap();
+  return server(event, context, callback);
+};
